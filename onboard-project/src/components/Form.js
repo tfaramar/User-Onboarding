ee3e85c838aa23c530dcffbea2b66a2f7b9f2607
tatiana faramarzi <tfaramarzi@gmail.com>
2019-08-07 14:34:21 -0700

    Added state to form, finished incorporating validation schema in form, rendering new users

--- onboard-project/src/components/Form.js
+++ onboard-project/src/components/Form.js
@@ -3,10 +3,17 @@ import axios from 'axios';
 import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
 
-const UserForm = () => {
+const UserForm = ({ errors, touched, values, handleSubmit, status }) => {
 
     const [users, setUsers] = useState([]);
     console.log(users);
+
+    useEffect(() => {
+        if (status) {
+            setUsers([...users, status]);
+        }
+    }, [status]);
+    console.log(status);
 
     return (
         <div className="user-form">
@@ -16,32 +23,47 @@ const UserForm = () => {
                     type="text"
                     name="name"
                     placeholder="Name"
-                    />
+                />
+                {touched.name && errors.name && (
+                    <p className="error">{errors.name}</p>
+                )}
 
                 <Field
-                   type="email"
-                   name="email"
-                   placeholder="Email" 
-                    />
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                />
+                 {touched.email && errors.email && (
+                    <p className="error">{errors.email}</p>
+                )}
 
                 <Field
-                   type="password"
-                   name="password"
-                   placeholder="Password" 
-                    />
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                />
+                 {touched.password && errors.password && (
+                    <p className="error">{errors.password}</p>
+                )}
 
                 <label className="checkbox-container">
                     Terms of Service
                     <Field
                         type="checkbox"
                         name="serviceterms"
-                        //checked={add checked value}
-                        />
+                        checked={values.serviceterms}
+                    />
+                    <span className="checkmark" />
                 </label>
 
                 <button type="submit">Submit</button>
 
             </Form>
+            <div className="users-list">
+                {users.map(user => (
+                    <p key={user.id}>{user.name}</p>
+                ))}
+            </div>
         </div>
     )
 
@@ -70,9 +92,17 @@ const FormikUserForm = withFormik({
             .required('Please enter a password.'),
         serviceterms: Yup.boolean()
 
-        
-
     }),
+
+    //Post to reqres.in test API on submit
+    handleSubmit(values, { setStatus }) {
+        axios.post('https://reqres.in/api/users/', values)
+            .then(res => {
+                //console.log(res.data);
+                setStatus(res.data);
+            })
+            .catch(error => console.log(error.response));
+    }
 
 
 })(UserForm);
